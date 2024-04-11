3e5aac3d89b78f30013d24ce4988de1642f64072
kimyoungsik <addi97@naver.com>
2024-04-11 11:45:34 +0900

    [refactoring] 50

--- Project15/PrimeFactor.cpp
+++ Project15/PrimeFactor.cpp
@@ -8,20 +8,25 @@ public:
 		vector<int> result = {};
 
 		if (num > 1) {
+
+			int divisor = 2;
+
 			if (num == 4) {
-				while (num % 2 == 0)
+				
+				
+				while (num % divisor == 0)
 				{
-					result.push_back(2);
-					num /= 2;
+					result.push_back(divisor);
+					num /= divisor;
 				}
 				return result;
 			}
 
 			if (num == 6) {
-				while (num % 2 == 0)
+				while (num % divisor == 0)
 				{
-					result.push_back(2);
-					num /= 2;
+					result.push_back(divisor);
+					num /= divisor;
 				}
 
 				while (num % 3 == 0)
