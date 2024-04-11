c17ef2ba90ba2f8e2b3a165911eba19f5cbae886
kimyoungsik <addi97@naver.com>
2024-04-11 11:15:34 +0900

    [feature] 37

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,10 +3,18 @@
 
 using namespace std;
 
-TEST(TestCaseName, TestName1) {
+TEST(TestCaseName, of1) {
 
 	PrimeFactor pf;
 	auto ret = pf.of(1);
 	vector<int> expected = {};
+	EXPECT_EQ(ret, expected);
+}
+
+TEST(TestCaseName, of2) {
+
+	PrimeFactor pf;
+	auto ret = pf.of(2);
+	vector<int> expected = {2};
 	EXPECT_EQ(ret, expected);
 }
