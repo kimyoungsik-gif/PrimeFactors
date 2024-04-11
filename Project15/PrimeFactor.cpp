506167b8fa75a63e73e7c5d2b10807d76612f616
kimyoungsik <addi97@naver.com>
2024-04-11 11:58:51 +0900

    [refactoring] 59

--- Project15/PrimeFactor.cpp
+++ Project15/PrimeFactor.cpp
@@ -7,19 +7,11 @@ public:
 	vector<int> of(int num) {
 		vector<int> result = {};
 
-		if (num > 1) {
-
-			if (num == 4 || num == 6 || num == 9 || num == 12) {
-				for (int divisor = 2; num > 1; divisor++) {
-					while (num % divisor == 0) {
-						result.push_back(divisor);
-						num /= divisor;
-					}
-				}
-				return result;
+		for (int divisor = 2; num > 1; divisor++) {
+			while (num % divisor == 0) {
+				result.push_back(divisor);
+				num /= divisor;
 			}
-
-			result.push_back(num);
 		}
 
 		return result;
